f516042fff3b38dfa943d2250d80d0dc1ce217f2
Sataapon Naowat <sataapon.n@kbtg.tech>
2021-03-10 12:54:13 +0700

    main.go can use shop package

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
-	"io"
-	"os"
+
+	"github.com/Sataapon/order_brushing/shop"
 )
 
 func main() {
@@ -13,26 +12,7 @@ func main() {
 
 func run() {
 	path := "dataset/order_brush_order.csv"
-	file, err := os.Open(path)
-	check(err)
-	r := csv.NewReader(file)
-	count := 0
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		check(err)
-		fmt.Println(record)
-		count++
-		if count == 5 {
-			break
-		}
-	}
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+	mapping := shop.New(path)
+	_ = mapping
+	fmt.Println("complete")
 }
